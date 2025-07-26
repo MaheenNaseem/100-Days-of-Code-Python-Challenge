8048c937fe16c0886c694e52da39517dbf8182b4
Maheen Naseem Azeemi <maheennaseem1779@gmail.com>
2025-07-26 20:28:03 +0500

    Create CE_LeapYear.py
    TODO: Add Functionality for no imput

---
+++ Day 10/CE_LeapYear.py
@@ -0,0 +1,22 @@
+def is_leap_year(year):
+  #TODO: Add functionality for no input
+    if year % 4 == 0:
+
+        if year % 100 == 0:
+
+            if year % 400 == 0:
+                leap_year = True
+
+            else:
+                leap_year = False
+        else:
+            leap_year = True
+
+    else:
+        leap_year = False
+    return leap_year
+
+print("LEAP YEAR OR NOT?\n")
+input_year = int(input("Enter a Year: \n"))
+output = is_leap_year(input_year)
+print(f"{input_year} was a leap year: {output}")
