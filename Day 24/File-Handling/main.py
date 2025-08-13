68d31ded11f057c283899e2de3c1a8346373924e
Maheen Naseem Azeemi <maheennaseem1779@gmail.com>
2025-08-13 19:09:01 +0500

    Add files via upload

---
+++ Day 24/File-Handling/main.py
@@ -0,0 +1,16 @@
+#opening the file and reading its content
+
+# with open("file.txt") as file:
+#     content = file.read()
+#     print(content)
+
+#write inside the file
+
+with open("file.txt", mode = "w") as file:
+    file.write("File was Overwrited.")
+
+#appending something inside the file
+
+with open("file.txt", mode = "a") as file:
+    file.write("\nFile was appended.")
+
