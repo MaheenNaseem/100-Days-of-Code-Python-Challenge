205179ca9aaba630b08a7e164af6b7bbde3f65d3
Maheen Naseem Azeemi <maheennaseem1779@gmail.com>
2025-09-11 15:37:44 +0500

    Update main.py

--- Day 26/NATO-alphabet-project/main.py
+++ Day 26/NATO-alphabet-project/main.py
@@ -9,32 +9,15 @@ nato_dict = {row.letter:row.code for (index, row) in nato_data.iterrows()}
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
-# Day 30: Added Error Handling for user input
-def generate_phonetic():
-    user_input = input("Enter a word: ").upper()
-    try:
-        # using list comprehension
-        code_words_list = [nato_dict[each_alphabet] for each_alphabet in user_input]
-    except KeyError:
-        error_message = "Sorry, only letters in the alphabet please."
-        print(error_message)
-        generate_phonetic()
-    else:
-        print(code_words_list)
+user_input = input("Enter your name: ").upper()
 
-generate_phonetic()
+#using list comprehension
+code_words_list = [nato_dict[each_alphabet] for each_alphabet in user_input]
 
-# Following is the code written on Day 26 for Nato Alphabet - Project
+#using for loop
+# code_words_list = []
+# for each_alphabet in user_input:
+#     code = nato_dict[each_alphabet]
+#     code_words_list.append(code)
 
-# user_input = input("Enter your name: ").upper()
-
-# #using list comprehension
-# code_words_list = [nato_dict[each_alphabet] for each_alphabet in user_input]
-
-# #using for loop
-# # code_words_list = []
-# # for each_alphabet in user_input:
-# #     code = nato_dict[each_alphabet]
-# #     code_words_list.append(code)
-
-# print(code_words_list)
+print(code_words_list)
