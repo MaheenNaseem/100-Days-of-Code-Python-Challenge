5d84441f2075bd6aaa2fc607e8e82450bf775967
Maheen Naseem Azeemi <maheennaseem1779@gmail.com>
2025-09-11 17:31:40 +0500

    Add Input validation

---
+++ Day 26/NATO-alphabet-project/main-v2.py
@@ -0,0 +1,27 @@
+# this version is revised after Day 30 - Add Error handling
+import pandas
+
+#TODO 1. Create a dictionary in this format:
+#{"A": "Alfa", "B": "Bravo"}
+
+nato_data = pandas.read_csv("nato_phonetic_alphabet.csv")
+
+# Keyword Method with iterrows()
+nato_dict = {row.letter:row.code for (index, row) in nato_data.iterrows()}
+
+#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+
+def generate_phonetic():
+    user_input = input("Enter a word: ").upper()
+    try:
+        # using list comprehension
+        code_words_list = [nato_dict[each_alphabet] for each_alphabet in user_input]
+    except KeyError:
+
+        error_message = "Sorry, only letters in the alphabet please."
+        print(error_message)
+
+    else:
+        print(code_words_list)
+
+generate_phonetic()
