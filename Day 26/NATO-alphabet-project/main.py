1159de3eb8c7c3667c4e6886308a7822508ad707
Maheen Naseem Azeemi <maheennaseem1779@gmail.com>
2025-09-11 15:39:58 +0500

    Implement error handling in main.py (Day 30 changes)

--- Day 26/NATO-alphabet-project/main.py
+++ Day 26/NATO-alphabet-project/main.py
@@ -9,15 +9,17 @@ nato_dict = {row.letter:row.code for (index, row) in nato_data.iterrows()}
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
-user_input = input("Enter your name: ").upper()
+# Day 30: Added Error Handling for user input
+def generate_phonetic():
+    user_input = input("Enter a word: ").upper()
+    try:
+        # using list comprehension
+        code_words_list = [nato_dict[each_alphabet] for each_alphabet in user_input]
+    except KeyError:
+        error_message = "Sorry, only letters in the alphabet please."
+        print(error_message)
+        generate_phonetic()
+    else:
+        print(code_words_list)
 
-#using list comprehension
-code_words_list = [nato_dict[each_alphabet] for each_alphabet in user_input]
-
-#using for loop
-# code_words_list = []
-# for each_alphabet in user_input:
-#     code = nato_dict[each_alphabet]
-#     code_words_list.append(code)
-
-print(code_words_list)
+generate_phonetic()
