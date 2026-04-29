1620c3c38b56befc5dd838e3f62d3d9d7b0bfc0b
Maheen <maheennaseem1779@gmail.com>
2026-04-29 14:15:29 +0500

    Step 02 Completed

--- Day 49/gym-class-booking-automation/main.py
+++ Day 49/gym-class-booking-automation/main.py
@@ -1,24 +1,34 @@
-from IPython.core.completerlib import import_re
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import os
 
-EMAIL= "test@gmail.com"
-PASSWORD= "" #your password here
-URL="https://appbrewery.github.io/gym/"
+EMAIL = "TEST@gmail.com"
+PASSWORD = "YOUR PASSWORD HERE"
+URL = "https://appbrewery.github.io/gym/"
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option('detach', True)
-driver = webdriver.Chrome(chrome_options)
-driver.get(URL)
 
-# create a directory in your project folder to store your Chrome Profile information
 user_data_dir = os.path.join(os.getcwd(), "chrome_profile")
-
-# Tell your Chrome Driver to use the directory you specified to store a "profile"
 chrome_options.add_argument(f"--user-data-dir={user_data_dir}")
 
+driver = webdriver.Chrome(options=chrome_options)
+driver.get(URL)
 
+# wait for elements to load
+driver.implicitly_wait(5)
 
+login_btn = driver.find_element(By.ID, 'login-button')
+login_btn.click()
 
+driver.implicitly_wait(2)
+
+email_field = driver.find_element(By.NAME, 'email')
+email_field.send_keys(EMAIL)
+
+password_field = driver.find_element(By.NAME, 'password')
+password_field.send_keys(PASSWORD)
+
+submit_btn = driver.find_element(By.ID, 'submit-button')
+submit_btn.click()
